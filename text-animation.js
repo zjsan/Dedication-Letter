2559032d757fc1abb84f8539a9fa82491e20c0f8
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2023-06-10 18:26:09 +0800

    Update text-animation.js

--- text-animation.js
+++ text-animation.js
@@ -6,6 +6,28 @@ document.addEventListener("DOMContentLoaded", function() {
     const intro3 = document.getElementById("intro3");
     const text1 = document.getElementById("text1");
     const text2 = document.getElementById("text2");
+    const text3 = document.getElementById("text3");
+    const text4 = document.getElementById("text4");
+    const text5 = document.getElementById("text5");
+    const text6 = document.getElementById("text6");
+    const text7 = document.getElementById("text7");
+    const text8 = document.getElementById("text8");
+    const text9 = document.getElementById("text9");
+    const text10 = document.getElementById("text10");
+    const text11 = document.getElementById("text11");
+    const text12 = document.getElementById("text12");
+    const text13 = document.getElementById("text13");
+    const text14 = document.getElementById("text14");
+    const text15 = document.getElementById("text15");
+    const text16 = document.getElementById("text16");
+    const text17 = document.getElementById("text17");
+    const text18 = document.getElementById("text18");
+    const text19 = document.getElementById("text19");
+    const text20 = document.getElementById("text20");
+    const text21 = document.getElementById("text21");
+    const text22 = document.getElementById("text22");
+    const text23 = document.getElementById("text23");
+    const text24 = document.getElementById("text24");
     const music = document.getElementById("play");
   
 
@@ -15,7 +37,7 @@ document.addEventListener("DOMContentLoaded", function() {
     music.addEventListener("playing", function()
   {
     
-    //4 seconds interval in the intro text
+    //6 seconds interval in the main text
     //intro1
     setTimeout(function() {
       intro1.classList.add("show");
@@ -41,17 +63,235 @@ document.addEventListener("DOMContentLoaded", function() {
 
     setTimeout(function() {
       intro3.classList.add("exit");
-    }, 11000);
+    }, 12000);
 
     //main text
     //text1
     setTimeout(function() {
       text1.classList.add("show");
-    }, 12000);
+    }, 13000);
 
     setTimeout(function() {
       text1.classList.add("exit");
-    }, 15000);
+    }, 19000);
+
+    //text2
+    setTimeout(function() {
+      text2.classList.add("show");
+    }, 20000);
+
+    setTimeout(function() {
+      text2.classList.add("exit");
+    }, 26000);
+
+    //text3
+    setTimeout(function() {
+      text3.classList.add("show");
+    }, 27000);
+
+    setTimeout(function() {
+      text3.classList.add("exit");
+    }, 34000);
+
+    //text4
+    setTimeout(function() {
+      text4.classList.add("show");
+    }, 36000);
+
+    setTimeout(function() {
+      text4.classList.add("exit");
+    }, 42000);
+
+    //text5
+    setTimeout(function() {
+      text5.classList.add("show");
+    }, 43000);
+
+    setTimeout(function() {
+      text5.classList.add("exit");
+    }, 48000);
+
+    //text6
+    setTimeout(function() {
+      text6.classList.add("show");
+    }, 49000);
+
+    setTimeout(function() {
+      text6.classList.add("exit");
+    }, 53000);
+
+    //text7
+    setTimeout(function() {
+      text7.classList.add("show");
+    }, 54000);
+
+    setTimeout(function() {
+      text7.classList.add("exit");
+    }, 58000);
+
+     //text8
+     setTimeout(function() {
+      text8.classList.add("show");
+    }, 59000);
+
+    setTimeout(function() {
+      text8.classList.add("exit");
+    }, 60000);
+
+     //text9
+     setTimeout(function() {
+      text9.classList.add("show");
+    }, 61000);
+
+    setTimeout(function() {
+      text9.classList.add("exit");
+    }, 64000);
+
+     //text10
+     setTimeout(function() {
+      text10.classList.add("show");
+    }, 65000);
+
+    setTimeout(function() {
+      text10.classList.add("exit");
+    }, 70000);
+
+     //text11
+     setTimeout(function() {
+      text11.classList.add("show");
+    }, 71000);
+
+    setTimeout(function() {
+      text11.classList.add("exit");
+    }, 76000);
+
+     //text12
+     setTimeout(function() {
+      text12.classList.add("show");
+    }, 77000);
+
+    setTimeout(function() {
+      text12.classList.add("exit");
+    }, 82000);
+
+     //text13
+     setTimeout(function() {
+      text13.classList.add("show");
+    }, 83000);
+
+    setTimeout(function() {
+      text13.classList.add("exit");
+    }, 87000);
+
+     //text14
+     setTimeout(function() {
+      text14.classList.add("show");
+    }, 88000);
+
+    setTimeout(function() {
+      text14.classList.add("exit");
+    }, 91000);
+
+     //text15
+     setTimeout(function() {
+      text15.classList.add("show");
+    }, 92000);
+
+    setTimeout(function() {
+      text15.classList.add("exit");
+    }, 94000);
+
+    //text16
+    setTimeout(function() {
+      text16.classList.add("show");
+    }, 95000);
+
+    setTimeout(function() {
+      text16.classList.add("exit");
+    }, 98000);
+
+    //text17
+    setTimeout(function() {
+      text17.classList.add("show");
+    }, 99000);
+
+    setTimeout(function() {
+      text17.classList.add("exit");
+    }, 102000);
+
+     //text18
+     setTimeout(function() {
+      text18.classList.add("show");
+    }, 103000);
+
+    setTimeout(function() {
+      text18.classList.add("exit");
+    }, 106000);
+
+    //text19
+    setTimeout(function() {
+      text19.classList.add("show");
+    }, 107000);
+
+    setTimeout(function() {
+      text19.classList.add("exit");
+    }, 120000);
+
+    //text20
+    setTimeout(function() {
+      text20.classList.add("show");
+    }, 113000);
+
+    setTimeout(function() {
+      text20.classList.add("exit");
+    }, 1180000);
+
+    //text21
+    setTimeout(function() {
+      text21.classList.add("show");
+    }, 119000);
+
+    setTimeout(function() {
+      text21.classList.add("exit");
+    }, 1220000);
+
+    //text22
+    setTimeout(function() {
+      text22.classList.add("show");
+    }, 123000);
+
+    setTimeout(function() {
+      text22.classList.add("exit");
+    }, 1250000);
+
+    //text23
+    setTimeout(function() {
+      text23.classList.add("show");
+    }, 124000);
+
+    setTimeout(function() {
+      text23.classList.add("exit");
+    }, 1280000);
+    
+    
+    //text24
+    setTimeout(function() {
+      text24.classList.add("show");
+    }, 129000);
+
+    setTimeout(function() {
+      text24.classList.add("exit");
+    }, 1320000);
+
+    //text25
+    setTimeout(function() {
+      text24.classList.add("show");
+    }, 133000);
+
+    setTimeout(function() {
+      text24.classList.add("exit");
+    }, 1370000);
+    
 
   });
 
