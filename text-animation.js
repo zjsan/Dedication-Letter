6562ca845965304c8bfd654d0d9a474a42578e79
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2023-06-10 17:22:01 +0800

    Update text-animation.js

--- text-animation.js
+++ text-animation.js
@@ -2,21 +2,58 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Get the elements
     const intro1 = document.getElementById("intro1");
+    const intro2 = document.getElementById("intro2");
+    const intro3 = document.getElementById("intro3");
     const text1 = document.getElementById("text1");
     const text2 = document.getElementById("text2");
+    const music = document.getElementById("play");
   
+
+    // Play the background music
+    music.play();
     // Add a class to the first paragraph after a delay
+    music.addEventListener("playing", function()
+  {
+    
+    //4 seconds interval in the intro text
+    //intro1
     setTimeout(function() {
-      text1.classList.add("show");
-    }, 2000);
-
+      intro1.classList.add("show");
+    }, 1000);
     setTimeout(function() {
-        text1.classList.add("exit");
-      }, 13000);
+        intro1.classList.add("exit");
+      }, 4000);
 
+    //intro2
     // Add a class to the second paragraph after a delay
     setTimeout(function() {
-      text2.classList.add("show");
+      intro2.classList.add("show");
+    }, 5000);
+
+    setTimeout(function() {
+      intro2.classList.add("exit");
+    }, 8000);
+
+    //intro 3
+    setTimeout(function() {
+      intro3.classList.add("show");
+    }, 9000);
+
+    setTimeout(function() {
+      intro3.classList.add("exit");
+    }, 11000);
+
+    //main text
+    //text1
+    setTimeout(function() {
+      text1.classList.add("show");
+    }, 12000);
+
+    setTimeout(function() {
+      text1.classList.add("exit");
     }, 15000);
+
   });
-  
+
+
+}); 
